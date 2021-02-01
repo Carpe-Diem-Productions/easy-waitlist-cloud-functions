96dd18e33e017453f1dbacf02eebf6c66a44a46b
joshingyou <github@joshyu.me>
2021-01-31 17:34:50 -0800

    Admin activation backend done

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,7 +5,5 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "google", "prettier"],
-  rules: {
-    quotes: ["error", "double"],
-  },
+  rules: {},
 };
--- functions/index.js
+++ functions/index.js
@@ -57,7 +57,7 @@ exports.replenishActivationCodes = functions.database
         // inside a Functions such as writing to Firestore.
         return snapshot.ref.parent
           .child(generatedCode)
-          .set({ code: generatedCode });
+          .set({ "reserved-for-uid": "" });
       })
       .catch((error) => {
         return error;
@@ -80,23 +80,110 @@ exports.addClaimsToNewUsers = functions.auth.user().onCreate((user) => {
     };
   }
 
+  functions.logger.debug(
+    "UID: ",
+    user.uid,
+    "Email: ",
+    user.email,
+    "Phone number: ",
+    user.phoneNumber,
+    "Custom claims: ",
+    JSON.stringify(customClaims)
+  );
+
   // Set custom user claims on this newly created user.
-  return admin
-    .auth()
-    .setCustomUserClaims(user.uid, customClaims)
-    .then(() => {
-      // Update real-time database to notify client to force refresh.
-      const metadataRef = admin.database().ref("metadata/" + user.uid);
-      // Set the refresh time to the current UTC timestamp.
-      // This will be captured on the client to force a token refresh.
-      return metadataRef.set({ refreshTime: new Date().getTime() });
-    })
-    .catch((error) => {
-      console.log(error);
-      return error;
-    });
+  return admin.auth().setCustomUserClaims(user.uid, customClaims);
+  // .then(() => {
+  //   // Update real-time database to notify client to force refresh.
+  //   const metadataRef = admin.database().ref("metadata/" + user.uid);
+  //   // Set the refresh time to the current UTC timestamp.
+  //   // This will be captured on the client to force a token refresh.
+  //   return metadataRef.set({ refreshTime: new Date().getTime() });
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  //   return error;
+  // });
 });
 
-// TODD
-// [ ]exports.receiveUidAsAdmin
-// [ ]exports.receiveUidAsUser
+// activate an admin user
+exports.activateAdminUser = functions.https.onCall((data, context) => {
+  // activation code passed from the client.
+  const suppliedCode = data.suppliedActivationCode;
+  // Authentication / user information is automatically added to the request.
+  const uid = context.auth.uid;
+
+  // Checking attribute.
+  if (typeof suppliedCode != "string" || suppliedCode.length != 12) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "The function must be called with " +
+        'one arguments "suppliedCode" containing the activation code.'
+    );
+  }
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  // Saving the new message to the Realtime Database.
+  const activationCodeRef = admin
+    .database()
+    .ref("/activation-codes/" + suppliedCode); // Sanitize the message.
+
+  return activationCodeRef
+    .once("value")
+    .then((dataSnapshot) => {
+      if (dataSnapshot.val() === null) {
+        throw new functions.https.HttpsError(
+          "not-found",
+          "This activation code doesn't appear to be valid."
+        );
+      }
+
+      const reservedForUid = dataSnapshot.val()["reserved-for-uid"];
+      if (reservedForUid !== uid) {
+        throw new functions.https.HttpsError(
+          "failed-precondition",
+          "You are using an activation code that's intended for another account. " +
+            "Please report this error to the website owner " +
+            "and include the following information: " +
+            " UID: " +
+            uid +
+            " Activation Code: " +
+            suppliedCode
+        );
+      }
+
+      const customClaims = {
+        could_see_admin: true,
+        admin_activated: true,
+      };
+
+      return admin.auth().setCustomUserClaims(uid, customClaims);
+    })
+    .then(() => {
+      return activationCodeRef.remove();
+    })
+    .then(() => {
+      return admin
+        .database()
+        .ref("/unverified-admins/" + uid)
+        .remove();
+    })
+    .then(() => {
+      const successMsg =
+        "Activated uid " + uid + "with activation code " + suppliedCode;
+      functions.logger.log(successMsg);
+      return { activated: true };
+    })
+    .catch((error) => {
+      // Re-throwing the error as an HttpsError so that the client gets the error details.
+      throw new functions.https.HttpsError(error.code, error.message, error);
+    });
+});
